890d70655282cbd8bcdace546e141eeacd11eb67
Ethan Gill <ethan.gill@me.com>
2015-09-30 13:34:09 -0400

    Revert "Merge branch 'master' of https://github.com/MazinZ/UK-ACM-website"
    This reverts commit 05725cbce3fae73eefa59089c5e7b2192a72092f, reversing
    changes made to 81f005d1f1e755875b0528bfb8fa12302fe39585.

--- cathacks/js/main.js
+++ cathacks/js/main.js
@@ -15,7 +15,6 @@ function calculateHomeSize() {
         $('#home').css('height', window.innerHeight);
     } else {
         $('#home').css('height', window.innerHeight - $('#nav-wrapper').height());
-		//$('.particles-js-canvas-el').css('width', )
     }
 	
 }
@@ -31,7 +30,6 @@ function affix() {
 $(window).resize(function() {
     var viewportWidth = $(window).width();
 	calculateHomeSize();
-	
 
 		//$('#nav-wrapper').height($("#navid").height());
 //$("#nav-wrapper").css({"min-height":$("#navid").height()+"px"});
@@ -44,134 +42,16 @@ $("#navid li").click(function(e) {
 		var idName = "#" + $(this).attr('id').toLowerCase();
 		if (idName == "#home" || idName == "#sponsors" || idName=="#apply"){
 			$('html, body').animate({
-				scrollTop: $(idName).offset().top -99
-			}, 1000);
-		}
-		else if (idName == "#faq"){
-			idName+="id";
-			$('html, body').animate({
-				scrollTop: $(idName).offset().top - .24*$(window).height()
+				scrollTop: $(idName).offset().top -100
 			}, 1000);
 		}
 		else {
 			idName+="id";
 			$('html, body').animate({
-				scrollTop: $(idName).offset().top - .35*$(window).height()
+				scrollTop: $(idName).offset().top - .24*$(window).height()
 			}, 1000);
+			
 		}
 });
-
-
-particlesJS("home", {
-  "particles": {
-    "number": {
-      "value": 60,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#c6c6c6"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 9,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#c6c6c6", //c6c6c6 //e1e0e0
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 1,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-
 
 
